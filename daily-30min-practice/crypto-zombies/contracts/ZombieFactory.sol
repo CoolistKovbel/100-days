88f08dbcdbb8a6b138ea89f121b1be56b7b50ef7
coolistkovbel <coolistkovbel@gmail.com>
2022-10-17 11:39:11 -0400

    struggling

--- daily-30min-practice/crypto-zombies/contracts/ZombieFactory.sol
+++ daily-30min-practice/crypto-zombies/contracts/ZombieFactory.sol
@@ -3,6 +3,8 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
+    event NewZombie(uint zombieId, string name, uint dna);
+
     uint dnaDigits = 16;
     uint dnaModulus = 10**dnaDigits;
 
@@ -13,8 +15,14 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    mapping(uint => address) public zombieToOwner;
+    mapping(address => uint) public ownerZombieCount;
+
     function _createZombie(string memory _name, uint _dna) private {
-        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string memory _str)
@@ -22,6 +30,13 @@ contract ZombieFactory {
         view
         returns (uint)
     {
-        // start here https://cryptozombies.io/en/lesson/1/chapter/11
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0, "You already have a zombie");
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
     }
 }
--- solidity/course/multisig-wallet/wallet/test/wallet.js
+++ solidity/course/multisig-wallet/wallet/test/wallet.js
@@ -1,4 +1,5 @@
-const { assert } = require("console");
+const { expectRevert } = require("@openzeppelin/test-helpers");
+const { web3 } = require("@openzeppelin/test-helpers/src/setup");
 
 const Wallet = artifacts.require("Wallet");
 
@@ -20,18 +21,78 @@ contract("Wallet", (accounts) => {
 
     assert(approvers.length === 3);
     assert(approvers[0] === accounts[0]);
-    assert(approvers[0] === accounts[1]);
-    assert(approvers[0] === accounts[2]);
-    assert(quorum.toString() === "2");
+    assert(approvers[1] === accounts[1]);
+    assert(approvers[2] === accounts[2]);
+    assert(quorum.toNumber() === 2);
   });
 
   it("Should create transfer", async () => {
     await wallet.createTransfer(100, accounts[5], { from: accounts[0] });
     const transfers = await wallet.viewTransfers();
+
     assert(transfers.length === 1);
-    assert(transfers[0].id.toString() === 100);
+    assert(transfers[0].id === "0");
+    assert(transfers[0].amount === "100");
     assert(transfers[0].to === accounts[5]);
-    assert(transfers[0].approvals.toString() === "1");
+    assert(transfers[0].approvals === "0");
     assert(transfers[0].sent === false);
+  });
+
+  it("should not create transfers", async () => {
+    await expectRevert(
+      wallet.createTransfer(100, accounts[5], { from: accounts[4] }),
+      "You are not allowed - only approvers"
+    );
+  });
+
+  it("should increment approvals", async () => {
+    await wallet.createTransfer(100, accounts[5], { from: accounts[0] });
+    await wallet.approveTransfer(0, { from: accounts[0] });
+    const transfers = await wallet.viewTransfers();
+    const balance = await web3.eth.getBalance(wallet.address);
+
+    assert(transfers[0].approvals === "1");
+    assert(transfers[0].sent === false);
+    assert(balance === "1000");
+  });
+
+  it("should send transfer if quorum reached", async () => {
+    const balanceBefore = web3.utils.toBN(
+      await web3.eth.getBalance(accounts[6])
+    );
+    await wallet.createTransfer(100, accounts[6], { from: accounts[0] });
+    await wallet.approveTransfer(0, { from: accounts[0] });
+    await wallet.approveTransfer(0, { from: accounts[1] });
+    const balanceAfter = await web3.utils.toBN(
+      await web3.eth.getBalance(accounts[6])
+    );
+    assert(balanceAfter.sub(balanceBefore).toNumber() === 100);
+  });
+
+  it("should not approve transfer if sender is not approved", async () => {
+    await wallet.createTransfer(100, accounts[5], { from: accounts[0] });
+    await expectRevert(
+      wallet.approveTransfer(0, { from: accounts[4] }),
+      "You are not allowed - only approvers"
+    );
+  });
+
+  it("should NOT approve transfer", async () => {
+    await wallet.createTransfer(100, accounts[6], { from: accounts[0] });
+    await wallet.approveTransfer(0, { from: accounts[0] });
+    await wallet.approveTransfer(0, { from: accounts[1] });
+    await expectRevert(
+      wallet.approveTransfer(0, { from: accounts[2] }),
+      "Transfer has already been sent"
+    );
+  });
+
+  it("should not approve transfer twice", async () => {
+    await wallet.createTransfer(100, accounts[6], { from: accounts[0] });
+    await wallet.approveTransfer(0, { from: accounts[0] });
+    await expectRevert(
+      wallet.approveTransfer(0, { from: accounts[0] }),
+      "cannot approve transfer twice"
+    );
   });
 });
